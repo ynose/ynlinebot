a22bc44aa22b7e0cf97596ab59b23910a0d838bd
Nose Yoshio <ynose@Nose-no-MacBook.local>
2017-02-25 16:44:18 +0900

    Blynk修正

--- index.php
+++ index.php
@@ -81,20 +81,18 @@ if ($event == 'postback') {
 
   // blynkでLEDをつける
   $blynk = curl_init();
-  curl_setopt($blynk, CURLOPT_URL, "https://blynk-cloud.com/753525ca17b54e83add9df0c635266c6/update/D0?value=1");
+  curl_setopt($blynk, CURLOPT_URL, "https://blynk-cloud.com/753525ca17b54e83add9df0c635266c6/update/" . $pin . "?value=1");
   curl_setopt($blynk, CURLOPT_CUSTOMREQUEST, 'GET');
-  curl_setopt($blynk, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($blynk, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($blynk, CURLOPT_HEADER, FALSE);
 
   $response = curl_exec($blynk);
   curl_close($blynk);
   
-  return;
-  // $response_format = [
-  //   "type" => "text",
-  //   "text" => $led . ' (' . $pin . ')'
-  // ];
+  $response_format = [
+    "type" => "text",
+    "text" => $led . ' (' . $pin . ')'
+  ];
 
 }
 
