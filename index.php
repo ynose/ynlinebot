d523b39831ddc35e7b7e537cd76123e46e1f61d6
Nose Yoshio <ynose@Nose-no-MacBook.local>
2017-02-25 16:14:17 +0900

    CURLOPT_CUSTOMREQUESTを追加

--- index.php
+++ index.php
@@ -82,6 +82,8 @@ if ($event == 'postback') {
   // blynkでLEDをつける
   $blynk = curl_init();
   curl_setopt($blynk, CURLOPT_URL, "https://blynk-cloud.com/753525ca17b54e83add9df0c635266c6/update/D0?value=1");
+  curl_setopt($blynk, CURLOPT_CUSTOMREQUEST, 'GET');
+  curl_setopt($blynk, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($blynk, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($blynk, CURLOPT_HEADER, FALSE);
 
