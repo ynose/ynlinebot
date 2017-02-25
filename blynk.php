06d8fe0268efa9e2de153721418663f7cedcd041
Nose Yoshio <ynose@Nose-no-MacBook.local>
2017-02-25 16:22:31 +0900

    Blynkテストを追加

---
+++ blynk.php
@@ -0,0 +1,16 @@
+<?php
+
+  // blynkでLEDをつける
+  $blynk = curl_init();
+  curl_setopt($blynk, CURLOPT_URL, "https://blynk-cloud.com/753525ca17b54e83add9df0c635266c6/update/D0?value=1");
+  curl_setopt($blynk, CURLOPT_CUSTOMREQUEST, 'GET');
+  curl_setopt($blynk, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($blynk, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($blynk, CURLOPT_HEADER, FALSE);
+
+  $response = curl_exec($blynk);
+  curl_close($blynk);
+  
+  return;
+
+?>
