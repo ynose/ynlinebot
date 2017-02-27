eadcf27d06d7cfd9651c109497d97f435f4a79b1
Yoshio Nose <ynose.249@gmail.com>
2017-02-27 11:17:00 +0900

    CURLOPT_USERAGENTを追加

--- blynk.php
+++ blynk.php
@@ -3,7 +3,8 @@
 
 //   // blynkでLEDをつける
   $blynk = curl_init();
-  curl_setopt($blynk, CURLOPT_URL, 'https://blynk-cloud.com/' . urlencode("753525ca17b54e83add9df0c635266c6") . '/update/D0?value=1');
+  curl_setopt($blynk, CURLOPT_URL, 'https://blynk-cloud.com/753525ca17b54e83add9df0c635266c6/update/D0?value=1');
+  curl_setopt($blynk, CURLOPT_USERAGENT, 'Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3B48b Safari/419.3');
   curl_setopt($blynk, CURLOPT_CUSTOMREQUEST, 'GET');
   curl_setopt($blynk, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($blynk, CURLOPT_HEADER, FALSE);
