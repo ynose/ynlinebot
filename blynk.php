51cfca56f8a9baec603535e5a5bbde7f0035294b
Yoshio Nose <ynose.249@gmail.com>
2017-02-27 14:13:05 +0900

    CURLOPT_URLを変更

--- blynk.php
+++ blynk.php
@@ -3,9 +3,9 @@
 
 //   // blynkでLEDをつける
   $blynk = curl_init();
-//  curl_setopt($blynk, CURLOPT_URL, 'http://blynk-cloud.com/753525ca17b54e83add9df0c635266c6/update/D0?value=1');
-  curl_setopt($blynk, CURLOPT_URL, 'http://cloud.blynk.cc:8080/753525ca17b54e83add9df0c635266c6/update/D0?value=1');
-  curl_setopt($blynk, CURLOPT_USERAGENT, 'Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3B48b Safari/419.3');
+  curl_setopt($blynk, CURLOPT_URL, 'https://blynk-cloud.com/753525ca17b54e83add9df0c635266c6/update/D0?value=1');
+//  curl_setopt($blynk, CURLOPT_URL, 'http://cloud.blynk.cc:8080/753525ca17b54e83add9df0c635266c6/update/D0?value=1');
+  curl_setopt($blynk, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)');
   curl_setopt($blynk, CURLOPT_SSL_VERIFYPEER, FALSE);
   curl_setopt($blynk, CURLOPT_VERBOSE, TRUE);
   curl_setopt($blynk, CURLOPT_CUSTOMREQUEST, 'GET');
